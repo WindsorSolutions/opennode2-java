0f019273e8790fcae0f54d619e5ea5e1fe6dc919
Christopher M. Miles <twitch@nervestaple.com>
2018-11-27 13:57:16 -0500

    Fixed issue saving data, worked on form layout

--- node-ng/node-web/src/main/java/com/windsor/node/web/content/exchange/ExchangeFormPanel.java
+++ node-ng/node-web/src/main/java/com/windsor/node/web/content/exchange/ExchangeFormPanel.java
@@ -111,14 +111,17 @@ public class ExchangeFormPanel extends ModalizablePanel<EditExchangeBean> {
 
         form.add(new Label("labelProtected", new IdentifiableResourceModel(NodeResourceModelKeys.LABEL_PROTECTED_TEXT)));
 
+        form.add(new Label("labelFiles",
+                new IdentifiableResourceModel(NodeResourceModelKeys.LABEL_FILES)));
+
         form.add(new RequirableFormGroup("filesGroup")
                 .add(new CheckBox("field", EditExchangeBeanModels.bindAutoDeleteFiles(getModel())))
                 .add(new Label("label", new IdentifiableResourceModel(NodeResourceModelKeys.LABEL_FILES_DELETE))));
 
         form.add(new RequirableFormGroup("filesAgeGroup")
                 .add(new TextField<>("field", EditExchangeBeanModels.bindAutoDeleteFileAge(getModel()))
-                        .setLabel(new IdentifiableResourceModel(NodeResourceModelKeys.LABEL_FILES_AGE))
-                        .add(new InputBehavior())));
+                        .add(new InputBehavior()))
+                .add(new Label("label", new IdentifiableResourceModel(NodeResourceModelKeys.LABEL_FILES_AGE))));
 
         form.add(new Label("labelPluginInformation",
                 new IdentifiableResourceModel(NodeResourceModelKeys.LABEL_PLUGIN_INFORMATION)));
