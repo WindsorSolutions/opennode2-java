cc8c1fcb9094e2045385e21823d195c37839ec74
ageery <andrew_geery@windsorsolutions.com>
2019-06-18 22:54:49 -0400

    Sync'ed SQL scripts.

--- node-plugin/node-plugin-rcra57-outbound/src/main/assembly/outer/RCRA_5.7-ORA-DDL.sql
+++ node-plugin/node-plugin-rcra57-outbound/src/main/assembly/outer/RCRA_5.7-ORA-DDL.sql
@@ -1,9 +1,50 @@
---------------------------------------------------------
---  File created - Monday-June-10-2019   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Table RCRA_CA_AREA
---------------------------------------------------------
+/*
+Copyright (c) 2016, The Environmental Council of the States (ECOS)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+ * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+ * Neither the name of the ECOS nor the names of its contributors may
+   be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*****************************************************************************************************************************
+ *
+ *  Script Name:  RCRA_5.7-ORA-DDL.sql.sql
+ *
+ *  Company:  Windsor Solutions, Inc.
+ *
+ *  Purpose:  This script will build the database objects supporting the RCRA v5.7 flow
+ *
+ *  Maintenance:
+ *
+ *    Analyst         Date            Comment
+ *    ----------      ----------      ------------------------------------------------------------------------------
+ *    Windsor         06/18/2019      Created
+ *
+ ****************************************************************************************************************************
+ */
 
   CREATE TABLE "RCRA_CA_AREA" 
    (	"CA_AREA_ID" VARCHAR2(40 BYTE), 
